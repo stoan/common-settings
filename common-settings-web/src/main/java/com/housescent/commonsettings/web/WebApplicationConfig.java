5c25d71aa6a477059dee3d24310ec0a061f27d86
Siya Sosibo <stoan09@gmail.com>
2017-04-24 16:08:02 +0200

    Updated Project Structure

--- common-settings-web/src/main/java/com/housescent/commonsettings/web/WebApplicationConfig.java
+++ common-settings-web/src/main/java/com/housescent/commonsettings/web/WebApplicationConfig.java
@@ -1,5 +1,6 @@
 package com.housescent.commonsettings.web;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -7,7 +8,10 @@ import org.springframework.context.annotation.ComponentScan;
  * Created by Siya Sosibo on 08-Nov-15.
  */
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.housescent.commonsettings.service"})
+@ComponentScan(basePackages = {"com.housescent.commonsettings"})
 public class WebApplicationConfig {
 
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplicationConfig.class, args);
+    }
 }
