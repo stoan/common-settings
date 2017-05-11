3e36e2e2ad4db1acda4745bf1a9330269a098028
Siya Sosibo <stoan09@gmail.com>
2017-05-11 16:36:19 +0200

    Added Integration tests

--- common-settings-web/src/test/java/com/housescent/commonsettings/web/controller/api/ClientControllerTest.java
+++ common-settings-web/src/test/java/com/housescent/commonsettings/web/controller/api/ClientControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 /**
  * Created by Siya Sosibo on 2017/03/01.
@@ -18,7 +19,7 @@ import static io.restassured.RestAssured.given;
 public class ClientControllerTest {
 
     @LocalServerPort
-    int port;
+    private int port;
 
     @Before
     public void setUp() throws Exception {
@@ -27,11 +28,14 @@ public class ClientControllerTest {
 
     @Test
     public void getProperty() throws Exception {
-        given().log().ifValidationFails().port(port).get("fetchValue?applicationName=TestAppName&key=key1").then().log().ifValidationFails().statusCode(200);
+        given().log().ifValidationFails().port(port).get("fetchValue?applicationName=TestAppName&key=key1").then()
+                .log().ifValidationFails().statusCode(200).assertThat().body(equalTo("value1"));
     }
 
     @Test
     public void getSettings() throws Exception {
+        given().log().ifValidationFails().port(port).get("fetchProperties?applicationName=TestAppName").then().log()
+                .ifValidationFails().statusCode(200).body("key2", equalTo("value2"));
 
     }
 
